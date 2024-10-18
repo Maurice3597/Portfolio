dc704fc9558ad428e323de813909d81894dc8908
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-10-18 17:01:44 +0100

    Used environmental variables to store email, password, hostname and portnumber

--- Contact_fxn.py
+++ Contact_fxn.py
@@ -1,19 +1,31 @@
 import smtplib, ssl
-import pandas
-def send_mail(message):
-    username = "datascienceandaienegneer@gmail.com"
-    password = 'ecri eafn hsxa wgqy'
+import pandas, os
+from dotenv import load_dotenv
 
-    host = 'smtp.gmail.com'
-    port = 465
+load_dotenv(".env")
+passkey = os.environ.get("PASSKEY")
+user_mail = os.environ.get("EMAIL")
+PORT= os.environ.get("PORT")
+HOST = os.environ.get("HOST")
+
+
+def send_mail(message, password =passkey, mail = user_mail):
+    password = passkey
+    user_email= mail
+
+    host = HOST
+    port = PORT
     receiver = "gobackenddevelopment@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context= context)as  server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
+        server.login(user_email, password)
+        server.sendmail(user_email, receiver, message)
 
 def topic():
     topics = pandas.read_csv("topics.csv")
     for index, topic in topics.iterrows():
         return topics["topic"]
 topic()
+
+
+print(f"{user_mail}\n{passkey}\n{HOST}\n{PORT}")
